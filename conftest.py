6190cd426cec541cbb5cfe9930e3d1cf403e8431
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-11-19 12:10:15 +0200

    after video 9_1

---
+++ conftest.py
@@ -0,0 +1,51 @@
+import pytest
+from fixture.application import Application
+import json
+import os.path
+import importlib
+
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
+    if web_config is None:
+        if request.config.getoption("--target_path") != "":
+            config_file = request.config.getoption("--target_path")
+        else:
+            config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            web_config = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--target_path", action="store", default="")
---
+++ test/test_login.py
@@ -0,0 +1,8 @@
+__author__ = 'Pysarev'
+
+
+
+
+def test_login(app):
+    app.session.login("administrator", "root")
+    assert app.session.is_logged_in_as("administrator")
